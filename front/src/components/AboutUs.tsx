67c9f066371b37f9612df198692b860b915e1838
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-31 11:49:47 +0300

    Move front end app to separate repo

--- front/src/components/AboutUs.tsx
+++
@@ -1,9 +0,0 @@
-import { Base } from "./Base";
-
-export const AboutUs = () => {
-  return (
-    <Base>
-      <h2>About Us</h2>
-    </Base>
-  );
-};
--- front/src/components/SignUp.tsx
+++
@@ -1,9 +0,0 @@
-import { Base } from "./Base";
-
-export const SignUp = () => {
-  return (
-    <Base>
-      <h2>Sign Up</h2>
-    </Base>
-  );
-};
